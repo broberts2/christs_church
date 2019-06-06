a52d3ebe0f4bcface83c58cc4d385a70fa76d0a7
broberts2 <broc.roberts@outlook.com>
2019-06-06 01:51:38 -0600

    changes

--- src/config.js
+++ src/config.js
@@ -2,12 +2,12 @@ module.exports = {
   fadeDelay: 500,
   fadeDuration: 1000,
   autoScrollToTop: true,
-  protocol: process.env.NODE_ENV === "production" ? "https://" : "http://",
+  protocol: process.env.NODE_ENV === "production" ? "https://" : "https://",
   api:
     process.env.NODE_ENV === "production"
       ? "christschurchmissoula.com"
-      : "localhost",
-  port: process.env.NODE_ENV === "production" ? "" : ":5000"
+      : "christschurchmissoula.com",
+  port: process.env.NODE_ENV === "production" ? "" : ""
   // protocol: "https://",
   // api: "christschurchmissoula.herokuapp.com",
   // port: ""
